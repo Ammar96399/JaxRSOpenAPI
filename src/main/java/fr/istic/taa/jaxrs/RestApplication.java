e0819d8da3e35f7e31dcf5b4f2afdd0645367a35
nep-nep327 <91326644+nep-nep327@users.noreply.github.com>
2022-09-12 22:26:51 +0200

    [HWAN] Added swagger library and config, issue with swagger that the local js/css resources cannot be load so used CDN resources instead

--- src/main/java/fr/istic/taa/jaxrs/RestApplication.java
+++ src/main/java/fr/istic/taa/jaxrs/RestApplication.java
@@ -1,6 +1,9 @@
 package fr.istic.taa.jaxrs;
 
+import fr.istic.taa.jaxrs.rest.AppointmentResource;
+import fr.istic.taa.jaxrs.rest.PatientResource;
 import fr.istic.taa.jaxrs.rest.ProfessionalResource;
+import fr.istic.taa.jaxrs.rest.SwaggerResource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 
 import javax.ws.rs.ApplicationPath;
@@ -20,7 +23,11 @@ public class RestApplication extends Application {
         resources.add(OpenApiResource.class);
 
         //Your own resources.
+        resources.add(PatientResource.class);
+        resources.add(AppointmentResource.class);
         resources.add(ProfessionalResource.class);
+
+        resources.add(SwaggerResource.class);
 
         return resources;
     }
